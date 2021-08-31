4c33955226b17e3a1f46ddecb59e3edeb2e15ffb
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-01 04:02:20 +0700

    make it better

---
+++ database/migrations/2021_08_31_205433_add_column_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnOrdersTable extends Migration
+{
+   /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('total_bill',255)->after('order_destroy');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('total_bill');
+        });
+    }
+}
