7a594df959cd84c49b9e09b5f3c17a89edf5bb3c
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-03 20:46:30 +0700

    make it better

--- resources/views/layouts/sale/layout-navBuilder.blade.php
+++ resources/views/layouts/sale/layout-navBuilder.blade.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fresh Mama</title>
-
+    <meta content='146969617613231' property='fb:app_id'/>
+    <meta content='https://www.facebook.com/thanhlong.ntlruby' property='fb:admins'/>
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
