5518db04057f6e7772ab06ebd28761cbb3b0049e
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-07 01:49:05 +0700

    make it better

--- resources/views/layouts/sale/layout.blade.php
+++ resources/views/layouts/sale/layout.blade.php
@@ -236,6 +236,22 @@
             $('#search_ajax').fadeOut();
         });
     </script> --}}
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/61366017649e0a0a5cd4e68c/1feu6en4b';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
