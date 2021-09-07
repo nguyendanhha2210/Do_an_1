3d607c62a3dc873625fe408b39ec4100d49094c3
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-08 01:05:50 +0700

    make it better

--- app/Http/Controllers/Sale/CheckoutController.php
+++ app/Http/Controllers/Sale/CheckoutController.php
@@ -381,7 +381,8 @@ class CheckoutController extends Controller
 
 
         $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-        $vnp_Returnurl = "https://fresh-mama.herokuapp.com/vnpay_php/vnpay_return.php";
+        $vnp_Returnurl = url('/vnpay_php/vnpay_return.php');
+        
         $vnp_TmnCode = "BHCR7T7U"; //Mã website tại VNPAY 
         $vnp_HashSecret = "LOZIHNNZNVGVOGSBDIZQXYKLBGIAJSYR"; //Chuỗi bí mật
 
