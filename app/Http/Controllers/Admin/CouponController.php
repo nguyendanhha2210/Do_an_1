8e9446290405bb658b55b46872f4d2e6e418d929
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-11 05:15:58 +0700

    make it better

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -138,12 +138,20 @@ class CouponController extends Controller
         //     })->groupBy('orders.customer_id')->orderBy(DB::raw('Sum(total_bill)'), 'desc')
         //     ->get();
 
-        $abc = Order::select('customer_id', DB::raw('SUM("total_bill") OVER (PARTITION BY customer_id) AS total_bill') )
-        ->where('order_status', '=', OrderStatus::SUCCESS)->with(['user'])
-        ->whereHas('user', function ($query) {
-            $query->where('role_id', RoleStateType::SALER);
-        })->groupBy('orders.customer_id')->orderBy('total_bill', 'desc')
-        ->get();
+        // $abc = Order::select('customer_id', DB::raw('SUM("total_bill") OVER (PARTITION BY customer_id) AS total_bill'))
+        //     ->where('order_status', '=', OrderStatus::SUCCESS)->with(['user'])
+        //     ->whereHas('user', function ($query) {
+        //         $query->where('role_id', RoleStateType::SALER);
+        //     })->groupBy('orders.customer_id')->orderBy('total_bill', 'desc')
+        //     ->get();
+
+        $abc = Order::select('customer_id', DB::raw('Sum(cast(total_bill as double totalBill))'))
+            ->where('order_status', '=', OrderStatus::SUCCESS)->with(['user'])
+            ->whereHas('user', function ($query) {
+                $query->where('role_id', RoleStateType::SALER);
+            })->groupBy('orders.customer_id')->orderBy('totalBill', 'desc')
+            ->get();
+
 
         $coupon = Coupon::where('id', $id)->first();
         $qualityCoupon = $coupon->time;
