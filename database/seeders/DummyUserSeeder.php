2dd229628ebeb423598c8cf4d116c5f50456e02a
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-18 23:18:51 +0700

    make it better

--- database/seeders/DummyUserSeeder.php
+++ database/seeders/DummyUserSeeder.php
@@ -18,9 +18,9 @@ class DummyUserSeeder extends Seeder
     {
         $date = Carbon::now();
         $dataSeeders = [
-            ['id' => 1, 'name' => 'Admin', 'email' => 'admin@gmail.com', 'phone' => '0912345678', 'phone' => 'avata-3.jpg', 'role_id' => '1', 'email_verified_at' => $date, 'password' => Hash::make('12345678'), 'reset_password_token' => 'NULL', 'reset_password_token_expire' => $date, 'remember_token' => null, 'created_at' => $date, 'updated_at' => $date],
-            ['id' => 2, 'name' => 'Shipper', 'email' => 'ship@gmail.com', 'phone' => '0912345678', 'phone' => 'avata-3.jpg', 'role_id' => '3', 'email_verified_at' => $date, 'password' => Hash::make('12345678'), 'reset_password_token' => 'NULL', 'reset_password_token_expire' => $date, 'remember_token' => null, 'created_at' => $date, 'updated_at' => $date],
-            ['id' => 3, 'name' => 'DanhHa', 'email' => 'danhha221020@gmail.com', 'phone' => '0912345678', 'phone' => 'avata-3.jpg', 'role_id' => '2', 'email_verified_at' => $date, 'password' => Hash::make('12345678'), 'reset_password_token' => 'NULL', 'reset_password_token_expire' => $date, 'remember_token' => null, 'created_at' => $date, 'updated_at' => $date],
+            ['id' => 1, 'name' => 'Admin', 'email' => 'admin@gmail.com', 'phone' => '0912345678', 'images' => 'avata-3.jpg', 'role_id' => '1', 'email_verified_at' => $date, 'password' => Hash::make('12345678'), 'reset_password_token' => 'NULL', 'reset_password_token_expire' => $date, 'remember_token' => null, 'created_at' => $date, 'updated_at' => $date],
+            ['id' => 2, 'name' => 'Shipper', 'email' => 'ship@gmail.com', 'phone' => '0912345678', 'images' => 'avata-3.jpg', 'role_id' => '3', 'email_verified_at' => $date, 'password' => Hash::make('12345678'), 'reset_password_token' => 'NULL', 'reset_password_token_expire' => $date, 'remember_token' => null, 'created_at' => $date, 'updated_at' => $date],
+            ['id' => 3, 'name' => 'DanhHa', 'email' => 'danhha221020@gmail.com', 'phone' => '0912345678', 'images' => 'avata-3.jpg', 'role_id' => '2', 'email_verified_at' => $date, 'password' => Hash::make('12345678'), 'reset_password_token' => 'NULL', 'reset_password_token_expire' => $date, 'remember_token' => null, 'created_at' => $date, 'updated_at' => $date],
         ];
         DB::table('users')->truncate();
         DB::table('users')->insert($dataSeeders);
