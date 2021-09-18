a577fa9b7d6c81484491c05cf05d54c7a7a5e796
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-18 22:19:49 +0700

    make it better

--- resources/js/components/Sale/Contact/ContactComponent.vue
+++ resources/js/components/Sale/Contact/ContactComponent.vue
@@ -28,10 +28,10 @@
           style="background-color: white; padding: 15px 10px"
         >
           <div class="contact-title">
-            <h4>Contacts Us ABCD</h4>
+            <h4>Contacts Us</h4>
             <p>
               Hãy liên hệ với chúng tôi nều bạn đang có nhưng băn khoăn và chưa
-              rõ về cách mua hàng .
+              rõ về cách mua hàng , đặt hàng bên chúng tôi.
             </p>
           </div>
           <div class="contact-widget">
