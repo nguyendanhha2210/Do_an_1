ab8295459dffcfa0ea8ea42c4a7ce04877b4f9b7
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-21 02:50:48 +0700

    make it better

---
+++ database/migrations/2021_09_20_195026_create_profits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('order_code');
+            $table->string('revenue');
+            $table->string('cost');
+            $table->double('profit');
+            $table->datetime('date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profits');
+    }
+}
