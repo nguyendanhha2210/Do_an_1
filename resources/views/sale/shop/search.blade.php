6268739695c5d51b1204733ac1893e93721edfa2
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-26 21:18:40 +0700

    make it better

--- resources/views/sale/shop/search.blade.php
+++ resources/views/sale/shop/search.blade.php
@@ -1,5 +1,15 @@
+{{-- @extends('layouts.sale.layout-navBuilder')
+@section('content')
+{{-- @dd($searchResult) --}}
+    {{-- <h1>klda;lkd;sakd;a</h1> --}}
+    {{-- <search-result :search-result="{{ json_encode($searchResult) }}"></search-result> --}}
+{{-- @endsection  --}}
+
 @extends('layouts.sale.layout-navBuilder')
 @section('content')
-    <h1>klda;lkd;sakd;a</h1>
-    {{-- <search-result :search-result="{{ json_encode($searchResult) }}"></search-result> --}}
+{{-- @dd($breadcrumbs) --}}
+ <h1>apodpadpad</h1>
+    {{-- <type-product :product-type="{{ json_encode($productType) }}"></type-product> --}}
 @endsection
+
+
