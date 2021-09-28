974247a68c63ff09defad3002c06d34f4968b730
Nguyen Danh Ha <danhha221020@gmail.com>
2021-09-28 11:44:38 +0700

    make it better

--- resources/js/components/Sale/Shop/shop/DataProducts.vue
+++ resources/js/components/Sale/Shop/shop/DataProducts.vue
@@ -174,7 +174,7 @@
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-dialog modal-lg" style="width: 85% !important;" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">
