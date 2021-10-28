cf9de2ddd5774b9871c35144aa01eec68e8a2598
Nguyen Danh Ha <danhha221020@gmail.com>
2021-10-28 22:49:17 +0700

    make it better

--- resources/js/components/Common/Chart/BarChart.vue
+++ resources/js/components/Common/Chart/BarChart.vue
@@ -12,20 +12,45 @@ export default {
     };
   },
   props: ["data"],
-  created: function () {
-  },
+  created: function () {},
   mounted() {
-     this.labels = this.options.map((item) => item.label);
+    // this.labels = this.options.map((item) => item.label);
     // var arr = []; (Cách 2)
     // this.options.forEach(element => {
     //   arr.push(element.label);
     // });
     // console.log('arr1', this.labels);
     // console.log('arr2', arr);
-    this.values = this.options.map((item) => item.value);
+    // this.values = this.options.map((item) => item.value);
     this.colors = this.options[0].color;
+    // console.log("values", this.options);
+    // for (let i = 0; i < this.options.length; i++) {
+    //   this.values[i] = this.options[i].value;
+    // }
 
-    // console.log("lable",this.labels);
+
+    this.labels = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    this.values = [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11];
+
+
+    // var values = [];
+    // this.options.forEach(element => {
+    //   values.push(element.value);
+    // });
+    // console.log("values2", this.values);
     // console.log("values",this.values);
     // console.log("colors",this.colors);
 
