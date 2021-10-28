fdce3b3eeebf734f0bf6021a8c7b2848e7cb57ba
Nguyen Danh Ha <danhha221020@gmail.com>
2021-10-28 22:31:32 +0700

    make it better

--- resources/js/components/Common/Chart/BarChart.vue
+++ resources/js/components/Common/Chart/BarChart.vue
@@ -1,78 +1,47 @@
-// <script>
-// import { Bar } from "vue-chartjs";
-
-// export default {
-//   extends: Bar,
-//   data() {
-//     return {
-//       lables: [],
-//       values: [],
-//       colors: "",
-//       options: this.data,
-//     };
-//   },
-//   props: ["data"],
-//   created: function () {
-//     this.labels = this.options.map((item) => item.label);
-//     // var arr = []; (Cách 2)
-//     // this.options.forEach(element => {
-//     //   arr.push(element.label);
-//     // });
-//     // console.log('arr1', this.labels);
-//     // console.log('arr2', arr);
-//     this.values = this.options.map((item) => item.value);
-//     this.colors = this.options[0].color;
-//   },
-//   mounted() {
-//     this.renderChart(
-//       {
-//         labels: this.labels,
-//         datasets: [
-//           {
-//             label: "Data One",
-//             backgroundColor: this.colors,
-//             data: this.values,
-//           },
-//         ],
-//       },
-//       { responsive: true, maintainAspectRatio: false }
-//     );
-//   },
-// };
-// </script>
-
 <script>
 import { Bar } from "vue-chartjs";
 
 export default {
   extends: Bar,
+  data() {
+    return {
+      lables: [],
+      values: [],
+      colors: "",
+      options: this.data,
+    };
+  },
+  props: ["data"],
+  created: function () {
+  },
   mounted() {
+     this.labels = this.options.map((item) => item.label);
+    // var arr = []; (Cách 2)
+    // this.options.forEach(element => {
+    //   arr.push(element.label);
+    // });
+    // console.log('arr1', this.labels);
+    // console.log('arr2', arr);
+    this.values = this.options.map((item) => item.value);
+    this.colors = this.options[0].color;
+
+    // console.log("lable",this.labels);
+    // console.log("values",this.values);
+    // console.log("colors",this.colors);
+
     this.renderChart(
       {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December"
-        ],
+        labels: this.labels,
         datasets: [
           {
             label: "Data One",
-            backgroundColor: "#f87979",
-            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-          }
-        ]
+            backgroundColor: this.colors,
+            data: this.values,
+          },
+        ],
       },
       { responsive: true, maintainAspectRatio: false }
     );
-  }
+  },
 };
 </script>
