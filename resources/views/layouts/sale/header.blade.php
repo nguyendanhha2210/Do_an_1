c0dc45d14be82ec8b8c006e1ea189b6ed8a257f1
Nguyen Danh Ha <danhha221020@gmail.com>
2022-02-20 00:42:28 +0700

    fix phone number

--- resources/views/layouts/sale/header.blade.php
+++ resources/views/layouts/sale/header.blade.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="phone-service">
                         <i class=" fa fa-phone"></i>
-                        +33 6868.6868
+                        0123456789
                     </div>
                     <div class="share-service">
                         <div id="fb-root"></div>
